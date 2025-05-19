6f7f41492371d6034832ea51231d2fd873a22c33
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-05-19 21:26:12 +0600

    database connected

--- index.js
+++ index.js
@@ -4,14 +4,46 @@ const cors = require("cors");
 const { config } = require("dotenv");
 const port = process.env.port || 3000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@meshal10613.mbbtx0s.mongodb.net/?retryWrites=true&w=majority&appName=meshal10613`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("Server is running......");
 });
 
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const usersCollection = client.db("a10-hobby-hatch").collection("users");
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port} ${process.env.DB_USER} ${process.env.DB_PASS}`);
+    console.log(`Server is running on port ${port}`);
 });
+
+// a10-hobby-hatch
+// EGdwz1bV76KkUljR
